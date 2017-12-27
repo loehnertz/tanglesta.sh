6a063797ea47fa5b3982afba91af60b606e0cd5a
loehnertz <jakob2102@gmail.com>
2017-12-27 15:18:00 +0100

    Added CSS styling for the tabs

--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -23,12 +23,96 @@
     export default {
         name: 'index',
         components: {RetrieveFromTangle, PersistToTangle, Settings},
-        methods: {
-
-        }
+        methods: {}
     }
 </script>
 
 <style>
+    h1 {
+        font-size: 2rem;
+        text-align: center;
+        margin-bottom: 1rem;
+    }
 
+    .tabs-component-tabs {
+        border: solid 1px #ddd;
+        border-radius: 6px;
+        margin-bottom: 5px;
+    }
+
+    .tabs-component-tab {
+        color: #999;
+        font-size: 14px;
+        font-weight: 600;
+        flex: 1;
+        margin-right: 0;
+        list-style: none;
+    }
+
+    .tabs-component-tab:hover {
+        color: #666;
+    }
+
+    .tabs-component-tab.is-active {
+        color: #000;
+    }
+
+    .tabs-component-tab.is-disabled * {
+        color: #cdcdcd;
+        cursor: not-allowed !important;
+    }
+
+    .tabs-component-tab-a {
+        display: block;
+        text-align: center;
+        color: inherit;
+        padding: .75em 1em;
+        text-decoration: none;
+    }
+
+    .tabs-component-panels {
+        padding: 4em 0;
+    }
+
+    @media (min-width: 700px) {
+        .tabs-component-tabs {
+            border: 0;
+            align-items: stretch;
+            display: flex;
+            justify-content: flex-start;
+            margin-bottom: -1px;
+        }
+
+        .tabs-component-tab {
+            position: relative;
+            z-index: 1;
+            background-color: #fff;
+            border: solid 1px #ddd;
+            border-radius: 3px 3px 0 0;
+            margin-right: .5em;
+            transform: translateY(2px);
+            transition: transform .5s ease;
+        }
+
+        .tabs-component-tab:last-child {
+            margin-right: 0;
+        }
+
+        .tabs-component-tab.is-active {
+            position: relative;
+            z-index: 3;
+            border-bottom: solid 1px #fff;
+            transform: translateY(0);
+        }
+
+        .tabs-component-panels {
+            position: relative;
+            z-index: 2;
+            background-color: #fff;
+            border: solid 1px #ddd;
+            border-radius: 0 0 6px 6px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .05);
+            padding: 4em 2em;
+        }
+    }
 </style>
