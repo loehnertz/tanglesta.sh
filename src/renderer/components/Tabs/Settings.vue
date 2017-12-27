7d067ffa0c503b61db9a2393e5c4f5476874cfeb
loehnertz <jakob2102@gmail.com>
2017-12-27 19:49:51 +0100

    Gave the 'Settings' component some more 'boxes'

--- src/renderer/components/Tabs/Settings.vue
+++ src/renderer/components/Tabs/Settings.vue
@@ -1,11 +1,47 @@
 <template>
     <section>
+        <div class="box">
+            <div class="field">
+                <p class="control has-icons-left">
+                    <input class="input is-medium" type="text" placeholder="Node-Provider" v-model="node">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-server"></i>
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="field has-addons">
+                <p class="control has-icons-left is-expanded">
+                    <input class="input is-medium" type="text" placeholder="Seed" v-model="seed"
+                           :disabled="autoSeed">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-cubes"></i>
+                    </span>
+                </p>
+                <p class="control">
+                    <span class="select is-medium">
+                        <select title="isEncrypted" v-model="autoSeed">
+                            <option value="true">automatic seed</option>
+                            <option value="false">custom seed</option>
+                        </select>
+                    </span>
+                </p>
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
     export default {
         name: 'settings',
+        data() {
+            return ({
+                node: '',
+                seed: '',
+                autoSeed: true,  // TODO: Toggling the property does not disable the seed input sometimes
+            });
+        },
         methods: {}
     }
 </script>
