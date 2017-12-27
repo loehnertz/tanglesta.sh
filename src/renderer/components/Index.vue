7155d2069f4c06d5872e78fa2af5fa3f804869a4
loehnertz <jakob2102@gmail.com>
2017-12-27 15:14:24 +0100

    Added the index component

---
+++ src/renderer/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <h1>tanglesta.sh</h1>
+        <tabs>
+            <tab name="Retrieve from Tangle">
+                RetrieveFromTangle
+            </tab>
+            <tab name="Persist to Tangle">
+                PersistToTangle
+            </tab>
+        </tabs>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'index',
+        components: {},
+        methods: {
+        }
+    }
+</script>
+
+<style>
+
+</style>
