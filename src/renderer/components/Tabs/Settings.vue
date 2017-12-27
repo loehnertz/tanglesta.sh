1c629ed8eaa1f734ff083f06235f0979f6b058c4
loehnertz <jakob2102@gmail.com>
2017-12-27 23:05:13 +0100

    Added labels to all the settings

--- src/renderer/components/Tabs/Settings.vue
+++ src/renderer/components/Tabs/Settings.vue
@@ -2,6 +2,7 @@
     <section>
         <div class="box">
             <div class="field">
+                <label class="label">Node-Provider</label>
                 <p class="control has-icons-left">
                     <input class="input is-medium" type="text" placeholder="Node-Provider" v-model="provider">
                     <span class="icon is-small is-left">
@@ -11,6 +12,7 @@
             </div>
         </div>
         <div class="box">
+            <label class="label">Seed</label>
             <div class="field has-addons">
                 <p class="control has-icons-left is-expanded">
                     <input class="input is-medium" type="text" placeholder="Seed"
@@ -32,6 +34,7 @@
             </div>
         </div>
         <div class="box">
+            <label class="label">Default Save Location</label>
             <div class="field has-addons">
                 <p class="control has-icons-left is-expanded">
                     <input class="input is-medium" type="text" placeholder="Default Save Location"
@@ -45,8 +48,10 @@
                 </p>
                 <p class="control">
                     <span class="select is-medium">
-                        <select title="defaultLocation" v-model="defaultLocation"
-                                v-on:change="resetDefaultSaveLocation">
+                        <select title="defaultLocation"
+                                v-model="defaultLocation"
+                                v-on:change="resetDefaultSaveLocation"
+                        >
                             <option :value="false">ask for every retrieval</option>
                             <option :value="true">set default location</option>
                         </select>
