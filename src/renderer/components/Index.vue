80de30301305a2704ae8d25aaebd4807d257afaf
loehnertz <jakob2102@gmail.com>
2017-12-27 15:17:05 +0100

    Added the three 'Tab' components to the 'Index' component

--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -3,20 +3,28 @@
         <h1>tanglesta.sh</h1>
         <tabs>
             <tab name="Retrieve from Tangle">
-                RetrieveFromTangle
+                <retrieve-from-tangle></retrieve-from-tangle>
             </tab>
             <tab name="Persist to Tangle">
-                PersistToTangle
+                <persist-to-tangle></persist-to-tangle>
+            </tab>
+            <tab name="Settings">
+                <settings></settings>
             </tab>
         </tabs>
     </main>
 </template>
 
 <script>
+    import RetrieveFromTangle from './Tabs/RetrieveFromTangle'
+    import PersistToTangle from './Tabs/PersistToTangle'
+    import Settings from './Tabs/Settings'
+
     export default {
         name: 'index',
-        components: {},
+        components: {RetrieveFromTangle, PersistToTangle, Settings},
         methods: {
+
         }
     }
 </script>
