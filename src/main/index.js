4c76365071747361a4c9c17ff5d438a12f7dde97
loehnertz <jakob2102@gmail.com>
2017-12-29 14:59:27 +0100

    Added a native menu to the application

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,11 @@
-import {app, BrowserWindow, ipcMain, dialog} from 'electron'
+import {
+    app,
+    BrowserWindow,
+    Menu,
+    ipcMain,
+    shell,
+    dialog
+} from 'electron'
 
 /**
  * Set `__static` path to static files in production
@@ -30,7 +37,101 @@ function createWindow() {
     });
 }
 
-app.on('ready', createWindow);
+function registerMenu() {
+    let template = [
+        {
+            label: 'Edit',
+            submenu: [
+                {role: 'undo'},
+                {role: 'redo'},
+                {type: 'separator'},
+                {role: 'cut'},
+                {role: 'copy'},
+                {role: 'paste'},
+                {role: 'pasteandmatchstyle'},
+                {role: 'delete'},
+                {role: 'selectall'}
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {role: 'reload'},
+                {type: 'separator'},
+                {role: 'resetzoom'},
+                {role: 'zoomin'},
+                {role: 'zoomout'},
+                {type: 'separator'},
+                {role: 'togglefullscreen'}
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                {role: 'minimize'},
+                {role: 'close'}
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click() {
+                        shell.openExternal('http://tanglesta.sh/')
+                    }
+                }
+            ]
+        }
+    ];
+
+    if (process.platform === 'darwin') {
+        // App menu
+        template.unshift({
+            label: app.getName(),
+            submenu: [
+                {role: 'about'},
+                {type: 'separator'},
+                {role: 'services', submenu: []},
+                {type: 'separator'},
+                {role: 'hide'},
+                {role: 'hideothers'},
+                {role: 'unhide'},
+                {type: 'separator'},
+                {role: 'quit'}
+            ]
+        });
+
+        // Edit menu
+        template[1].submenu.push(
+            {type: 'separator'},
+            {
+                label: 'Speech',
+                submenu: [
+                    {role: 'startspeaking'},
+                    {role: 'stopspeaking'}
+                ]
+            }
+        );
+
+        // Window menu
+        template[3].submenu = [
+            {role: 'close'},
+            {role: 'minimize'},
+            {role: 'zoom'},
+            {type: 'separator'},
+            {role: 'front'}
+        ];
+    }
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+}
+
+app.on('ready', () => {
+    createWindow();
+    registerMenu();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
